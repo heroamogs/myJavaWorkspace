e290cacf63fa57fec1eaffeb7e9fb9fcf9318220
Samuel Okolo <herookolo@gmail.com>
2023-05-14 08:31:55 +0100

    first commit

---
+++ Algorithm_Java/src/Recursion.java
@@ -0,0 +1,12 @@
+
+public class Recursion {
+
+	static int[] arr = {12, 2, 30, 13, 5};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sumOfDigits(arr.length-1)
+	}
+
+}
---
+++ Algorithm_Java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author USER
+ *
+ */
+module Algorithm_Java {
+}
