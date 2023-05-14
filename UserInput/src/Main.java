745b9f08138872e82be811d6ab1843213dcdbb10
Samuel Okolo <herookolo@gmail.com>
2023-05-14 11:54:42 +0100

    scanner commit

--- UserInput/src/Main.java
+++ UserInput/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String userName = myScanner.nextLine();
+        System.out.println("Thank you " + userName);
+
+        System.out.println("What are you currently doing " + userName + "?");
+        String jobDone = myScanner.nextLine();
+        System.out.println(jobDone + " is a great thing!");
     }
 }
